9ced8248fe22d76840831e42037a64c554d175c3
Deepak Kumar Ojha <Deepakjh632@gmail.com>
2020-07-12 12:59:13 +0530

    flash toggle button added

--- app/src/main/java/com/incrementors/handwritingcreator/CameraFragment.java
+++ app/src/main/java/com/incrementors/handwritingcreator/CameraFragment.java
@@ -4,10 +4,12 @@ import android.Manifest;
 import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.SurfaceHolder;
@@ -17,9 +19,12 @@ import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.TranslateAnimation;
+import android.widget.CompoundButton;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -28,15 +33,19 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 
-public class CemeraFragment extends Fragment implements SurfaceHolder.Callback {
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
 
+public class CameraFragment extends Fragment implements SurfaceHolder.Callback {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     private static final long ANIMATION_DURATION = 1000L;
-    private static final int CAMERA_REQUEST_CODE = 100;
+    private static final int CAMERA_AND_EXTERNAL_REQUEST_CODE = 100;
     private SurfaceView surfaceView;
     private SurfaceHolder surfaceHolder;
     private Camera.Parameters mParameters;
@@ -46,10 +55,13 @@ public class CemeraFragment extends Fragment implements SurfaceHolder.Callback {
     private FrameLayout frame;
     private CardView infoCard, request;
     private View view;
-    private FloatingActionButton captureButton;
+    private ImageView captureButton;
+    private ToggleButton flasbtn;
+    private Camera.PictureCallback pictureCallback;
 
-    public CemeraFragment() {
-        // Required empty public constructor
+
+    public CameraFragment() {
+
     }
 
     public static Camera getCameraInstance() {
@@ -65,7 +77,6 @@ public class CemeraFragment extends Fragment implements SurfaceHolder.Callback {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     public void animate(View v) {
@@ -80,7 +91,7 @@ public class CemeraFragment extends Fragment implements SurfaceHolder.Callback {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cemera, container, false);
+        return inflater.inflate(R.layout.fragment_camera, container, false);
     }
 
     @Override
@@ -90,7 +101,7 @@ public class CemeraFragment extends Fragment implements SurfaceHolder.Callback {
         infoCard = view.findViewById(R.id.alert_card);
         request = view.findViewById(R.id.request);
         if (hasCameraHardware(view.getContext())) {
-            checkPermission(Manifest.permission.CAMERA, CAMERA_REQUEST_CODE);
+            checkPermission(Manifest.permission.CAMERA, CAMERA_AND_EXTERNAL_REQUEST_CODE);
         } else {
             Toast.makeText(view.getContext(), "No camera hardware found", Toast.LENGTH_SHORT).show();
         }
@@ -104,7 +115,7 @@ public class CemeraFragment extends Fragment implements SurfaceHolder.Callback {
                 @Override
                 public void onClick(View v) {
                     animateView(v);
-                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, CAMERA_AND_EXTERNAL_REQUEST_CODE);
                 }
             });
         } else {
@@ -116,10 +127,9 @@ public class CemeraFragment extends Fragment implements SurfaceHolder.Callback {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == CAMERA_REQUEST_CODE) {
+        if (requestCode == CAMERA_AND_EXTERNAL_REQUEST_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(view.getContext(), "Camera Permission Granted", Toast.LENGTH_SHORT).show();
-                infoCard.setVisibility(View.GONE);
                 init(view);
             } else {
                 //Toast.makeText(this, " naa naa naa Camera Permission Denied", Toast.LENGTH_LONG).show();
@@ -133,16 +143,60 @@ public class CemeraFragment extends Fragment implements SurfaceHolder.Callback {
         captureButton = view.findViewById(R.id.captureBtn);
         text = view.findViewById(R.id.text);
         surfaceView = view.findViewById(R.id.surface);
+        flasbtn = view.findViewById(R.id.toggleFlash);
         animator = ObjectAnimator.ofFloat(text, View.ALPHA, 0f, 1f);
+        infoCard.setVisibility(View.GONE);
         frame.setVisibility(View.VISIBLE);
         createSurface();
+        pictureCallback = new Camera.PictureCallback() {
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+                File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+                if (pictureFile == null) {
+                    Log.d(String.valueOf(getContext()), "Error creating media file, check storage permissions");
+                    return;
+                }
+
+                try {
+                    FileOutputStream fos = new FileOutputStream(pictureFile);
+                    fos.write(data);
+                    fos.close();
+                } catch (FileNotFoundException e) {
+                    Log.d(String.valueOf(getContext()), "File not found: " + e.getMessage());
+                } catch (IOException e) {
+                    Log.d(String.valueOf(getContext()), "Error accessing file: " + e.getMessage());
+                }
+            }
+        };
         captureButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 animateView(v);
-                Toast.makeText(v.getContext(), "Capture button clicked", Toast.LENGTH_SHORT).show();
+                //dispatchTakePictureIntent();
             }
         });
+
+        flasbtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Toast.makeText(getContext(), "Checked", Toast.LENGTH_SHORT).show();
+                } else
+                    Toast.makeText(getContext(), "Not Checked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private File getOutputMediaFile(int mediaTypeImage) {
+        File file = null;
+        return file;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 
     public void createSurface() {
@@ -152,7 +206,7 @@ public class CemeraFragment extends Fragment implements SurfaceHolder.Callback {
     }
 
     public void animateView(final View v) {
-        v.animate().scaleX(0.98f).scaleY(0.98f).setDuration(20).withEndAction(new Runnable() {
+        v.animate().scaleX(0.80f).scaleY(0.80f).setDuration(20).withEndAction(new Runnable() {
             @Override
             public void run() {
                 v.animate().scaleX(1.0f).scaleY(1.0f).setDuration(20);
